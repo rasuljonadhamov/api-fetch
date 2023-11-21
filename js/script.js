99e9226ea0c962d928cf1dfe6e605eb6aa0100d3
rasuljonadhamov <rasuljonadhamov@gmail.com>
2023-11-21 19:49:16 +0300

    fetch api and display data

--- js/script.js
+++ js/script.js
@@ -8,11 +8,14 @@ fetch("https://restcountries.com/v3.1/all")
       const li = document.createElement("li");
       const img = document.createElement("img");
       const h1 = document.createElement("h1");
+      const h3 = document.createElement("h3");
       h1.textContent = country.name.common;
-      img.setAttribute("src", `${country.coatOfArms.png}`);
+      h3.textContent = country.capital;
+      img.setAttribute("src", `${country.flags.png}`);
 
       li.appendChild(img);
       li.appendChild(h1);
+      li.appendChild(h3);
       block.appendChild(li);
     });
   })
