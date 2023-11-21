8b7a4646cbbfe025b5ee0692b1ebe16f661829f8
rasuljonadhamov <rasuljonadhamov@gmail.com>
2023-11-21 20:37:36 +0300

    Add search functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 const block = document.getElementById("block");
+const search = document.getElementById("search");
+const btn = document.getElementById("btn");
+
+function searching() {
+  if (search.value) {
+    let searchQuery = search.value.toLowerCase();
+
+    // Get all list items
+    let items = block.getElementsByTagName("li");
+
+    // Loop through each item and hide/show based on the search query
+    for (let i = 0; i < items.length; i++) {
+      let itemText = items[i].innerText.toLowerCase();
+
+      // If the item contains the search query, show it; otherwise, hide it
+      if (itemText.includes(searchQuery)) {
+        items[i].style.display = "block";
+      } else {
+        items[i].style.display = "none";
+      }
+    }
+  }
+}
 
 fetch("https://restcountries.com/v3.1/all")
   .then((response) => response.json())
@@ -22,3 +45,12 @@ fetch("https://restcountries.com/v3.1/all")
   .catch((err) => {
     console.log(err);
   });
+
+search.addEventListener("input", function (e) {
+  searching();
+});
+
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  searching();
+});
