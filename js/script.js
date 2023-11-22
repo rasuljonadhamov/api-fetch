2658259ce951bf0456ef984594a23358f1394dd7
rasuljonadhamov <rasuljonadhamov@gmail.com>
2023-11-22 17:07:34 +0300

    refactor javascript code

--- js/script.js
+++ js/script.js
@@ -2,98 +2,89 @@ const block = document.getElementById("block");
 const search = document.getElementById("search");
 const btn = document.getElementById("btn");
 const continent = document.getElementById("qita");
+const showMoreButton = document.getElementById("show-more-btn");
+let countriesData = [];
+let displayedCountries = 10;
+let totalCountries = 0;
 
-function searching() {
-  if (search.value) {
-    let searchQuery = search.value.toLowerCase();
+function createCountryListItem(country) {
+  const li = document.createElement("li");
+  const img = document.createElement("img");
+  const h1 = document.createElement("h1");
+  const h3 = document.createElement("h3");
+  const p = document.createElement("p");
+  h1.textContent = country.name.common;
+  h3.textContent = country.capital;
+  p.textContent = country.continents;
+  img.setAttribute("src", `${country.flags.png}`);
 
-    // Get all list items
-    let items = block.getElementsByTagName("li");
+  li.appendChild(img);
+  li.appendChild(h1);
+  li.appendChild(h3);
+  li.appendChild(p);
+  return li;
+}
 
-    // Loop through each item and hide/show based on the search query
-    for (let i = 0; i < items.length; i++) {
-      let itemText = items[i].innerText.toLowerCase();
-
-      // If the item contains the search query, show it; otherwise, hide it
-      if (itemText.includes(searchQuery)) {
-        items[i].style.display = "block";
-      } else {
-        items[i].style.display = "none";
-      }
-    }
+// Function to display countries based on a provided array
+function displayCountries(countryArray, limit) {
+  block.innerHTML = "";
+  for (let i = 0; i < limit; i++) {
+    const li = createCountryListItem(countryArray[i]);
+    block.appendChild(li);
   }
 }
 
+// Fetch countries data and store it
 fetch("https://restcountries.com/v3.1/all")
   .then((response) => response.json())
   .then((json) => {
-    return json.forEach((country) => {
-      console.log(country);
-      const li = document.createElement("li");
-      const img = document.createElement("img");
-      const h1 = document.createElement("h1");
-      const h3 = document.createElement("h3");
-      const p = document.createElement("p");
-      h1.textContent = country.name.common;
-      h3.textContent = country.capital;
-      p.textContent = country.continents;
-      img.setAttribute("src", `${country.flags.png}`);
+    countriesData = json;
+    totalCountries = countriesData.length;
+    displayCountries(countriesData, displayedCountries);
 
-      li.appendChild(img);
-      li.appendChild(h1);
-      li.appendChild(h3);
-      li.appendChild(p);
-      block.appendChild(li);
-    });
+    if (totalCountries > displayedCountries) {
+      showMoreButton.style.display = "block";
+    }
   })
   .catch((err) => {
-    console.log(err);
+    console.error(err);
   });
 
-search.addEventListener("input", function (e) {
-  searching();
+showMoreButton.addEventListener("click", function () {
+  displayedCountries += 10; // Increase the count of displayed countries
+  if (displayedCountries >= totalCountries) {
+    showMoreButton.style.display = "block";
+  }
+  displayCountries(countriesData, displayedCountries);
 });
+
+function filterCountriesBySearch() {
+  const searchQuery = search.value.toLowerCase();
+
+  const filteredCountries = countriesData.filter((country) =>
+    country.name.common.toLowerCase().includes(searchQuery)
+  );
+
+  displayCountries(filteredCountries, displayedCountries);
+}
+
+search.addEventListener("input", filterCountriesBySearch);
 
 btn.addEventListener("click", function (e) {
   e.preventDefault();
-  searching();
+  filterCountriesBySearch();
 });
 
-continent.addEventListener("change", function (json) {
+continent.addEventListener("change", function () {
   const selectedCon = this.value;
 
-  fetch("https://restcountries.com/v3.1/all")
-    .then((response) => response.json())
-    .then((json) => {
-      let arr = [];
-      json.forEach((el) => {
-        if (el.continents[0] == selectedCon) {
-          arr.push(el);
-        }
-      });
+  if (selectedCon) {
+    const filteredCountries = countriesData.filter(
+      (country) => country.continents[0] === selectedCon
+    );
 
-      if (arr.length) {
-        block.innerHTML = "";
-        arr.forEach((country) => {
-          const li = document.createElement("li");
-          const img = document.createElement("img");
-          const h1 = document.createElement("h1");
-          const h3 = document.createElement("h3");
-          const p = document.createElement("p");
-          h1.textContent = country.name.common;
-          h3.textContent = country.capital;
-          p.textContent = country.continents;
-          img.setAttribute("src", `${country.flags.png}`);
-
-          li.appendChild(img);
-          li.appendChild(h1);
-          li.appendChild(h3);
-          li.appendChild(p);
-          block.appendChild(li);
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    displayCountries(filteredCountries, displayedCountries);
+  } else {
+    displayCountries(countriesData, displayedCountries);
+  }
 });
