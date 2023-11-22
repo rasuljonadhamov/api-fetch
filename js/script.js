aaee1f1e4a0eed9ba325264677da06600c4294ee
rasuljonadhamov <rasuljonadhamov@gmail.com>
2023-11-22 16:35:35 +0300

    Add styles

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ fetch("https://restcountries.com/v3.1/all")
   .then((response) => response.json())
   .then((json) => {
     return json.forEach((country) => {
-      console.log(country.continents);
+      console.log(country);
       const li = document.createElement("li");
       const img = document.createElement("img");
       const h1 = document.createElement("h1");
